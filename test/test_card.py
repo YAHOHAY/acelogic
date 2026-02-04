1fb1dd63e24e8c34061a0fa4c1e40ccf9d4a691e
biter <nlrsrzrcj@proton.me>
2026-02-04 08:43:47 -0500

    test

--- test/test_card.py
+++ test/test_card.py
@@ -1,6 +1,5 @@
 from ace_logic.core.card import Card, Suit, Rank
-
-c1 = Card(Suit.SPADES, Rank.ACE)
-c2 = Card(Suit.HEARTS, Rank.TEN)
-print(f"我的牌: {c1}, 你的牌: {c2}")
-print(f"我比你大吗？ {c1 > c2}")
+def test_suit_init():
+    c1 = Card(Suit.SPADES, Rank.ACE)
+    c2 = Card(Suit.HEARTS, Rank.TEN)
+    assert c1 > c2
